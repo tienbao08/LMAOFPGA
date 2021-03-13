496f26a55e1769a805dfcdea39e59be748f0351a
NguyenTienBao <47103241+tienbao08@users.noreply.github.com>
2021-03-14 00:35:46 +0700

    Add files via upload

--- assignment/week2/rgb2gray.py
+++ assignment/week2/rgb2gray.py
@@ -11,8 +11,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # cv2.imshow('Gray image', gray)
 # Y = 0.299 R + 0.587 G + 0.114 B
 cv2.imwrite("test_gray.jpg", gray)
-gray_pix = gray[0]
-print(red)
+gray_pix = gray.flatten()
 with open(r'test_gray.txt', 'w') as f:
     f.write("\n".join(map(str, gray_pix)))
 with open(r'red.txt', 'w') as f:
