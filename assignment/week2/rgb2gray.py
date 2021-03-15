a12a1847343c9350089f81c47e9f045c008e6f66
NguyenTienBao <47103241+tienbao08@users.noreply.github.com>
2021-03-15 11:42:54 +0700

    Add files via upload

--- assignment/week2/rgb2gray.py
+++ assignment/week2/rgb2gray.py
@@ -1,22 +1,31 @@
 import cv2
-import numpy as np
+import fire
 
-img = cv2.imread('test.jpg')
-blue = img[:, :, 0].flatten()  # Gives **Blue**
-green = img[:, :, 1].flatten()  # Gives **Green**
-red = img[:, :, 2].flatten()  # Gives **Red**
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# cv2.imshow('Original image', img_rgb)
-# cv2.imshow('Gray image', gray)
-# Y = 0.299 R + 0.587 G + 0.114 B
-cv2.imwrite("test_gray.jpg", gray)
-gray_pix = gray.flatten()
-with open(r'test_gray.txt', 'w') as f:
-    f.write("\n".join(map(str, gray_pix)))
-with open(r'red.txt', 'w') as f:
-    f.write("\n".join(map(str, red)))
-with open(r'green.txt', 'w') as f:
-    f.write("\n".join(map(str, green)))
-with open(r'blue.txt', 'w') as f:
-    f.write("\n".join(map(str, blue)))
+def rgb2gray(stockimg):
+    img = cv2.imread(stockimg)
+    blue = img[:, :, 0].flatten()  # Gives **Blue**
+    green = img[:, :, 1].flatten()  # Gives **Green**
+    red = img[:, :, 2].flatten()  # Gives **Red**
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Original image', img)
+    cv2.imshow('Gray image', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # Y = 0.299 R + 0.587 G + 0.114 B
+    cv2.imwrite("test_gray.jpg", gray)
+    gray_pix = gray.flatten()
+    with open(r'test_gray.txt', 'w') as f:
+        f.write("\n".join(map(str, gray_pix)))
+    with open(r'red.txt', 'w') as f:
+        f.write("\n".join(map(str, red)))
+    with open(r'green.txt', 'w') as f:
+        f.write("\n".join(map(str, green)))
+    with open(r'blue.txt', 'w') as f:
+        f.write("\n".join(map(str, blue)))
+    print(red, green, blue, gray_pix)
+
+
+if __name__ == '__main__':
+    fire.Fire(rgb2gray)
