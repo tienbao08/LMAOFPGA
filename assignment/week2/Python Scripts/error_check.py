ec305eaef44aaec4d518fed39a5e1492bb10dc1e
NguyenTienBao <47103241+tienbao08@users.noreply.github.com>
2021-03-26 14:20:17 +0700

    improve function

--- assignment/week2/Python Scripts/error_check.py
+++ assignment/week2/Python Scripts/error_check.py
@@ -1,20 +1,25 @@
 import numpy as np
 import fire
 import matplotlib.pyplot as plt
+import cv2
 h = 541
 w = 960
 
 
 def error_check(filename1, filename2):
-    cv_gray = np.loadtxt(''.join(('', str(filename1), '')), dtype=int)
-    hdl_gray = np.loadtxt(''.join(('', str(filename2), '')), dtype=int)
+    cv_gray = np.loadtxt(''.join(('', str(filename1), '')), dtype=np.uint8)
+    hdl_gray = np.loadtxt(''.join(('', str(filename2), '')), dtype=np.uint8)
     index = [0, 1]
     hdl_gray = np.delete(hdl_gray, index)
     error = cv_gray - hdl_gray
-    print(np.amax(error))
-    newarr = cv_gray.reshape(h, w)
-    plt.imshow(newarr, cmap='gray', vmin=0, vmax=255)
-    plt.show()
+    print("Max error: " + str(np.amax(error)))
+    cv_gray = cv_gray.reshape(h, w)
+    hdl_gray = hdl_gray.reshape(h, w)
+    cv2.imshow("cv_gray", cv_gray)
+    cv2.imshow("hdl_gray", hdl_gray)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
