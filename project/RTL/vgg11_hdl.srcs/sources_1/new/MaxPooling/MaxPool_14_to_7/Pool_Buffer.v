e912ebd964e1a9accb65cce31b445aed6f7cfee8
DucNguyen008 <76895207+DucNguyen008@users.noreply.github.com>
2021-05-24 11:43:28 +0700

    Add files via upload

---
+++ project/RTL/vgg11_hdl.srcs/sources_1/new/MaxPooling/MaxPool_14_to_7/Pool_Buffer.v
@@ -0,0 +1,63 @@
+module Pool_Buffer
+#(
+	parameter DATA_WIDTH = 32,
+	parameter ADDR_WIDTH = 4 
+)
+(
+	input clk, reset,
+	input [DATA_WIDTH-1:0] Din,
+	output [DATA_WIDTH-1:0] o12,
+	output reg [DATA_WIDTH-1:0] o11, o21, o22,
+	output valid_out,
+	output [DATA_WIDTH-1:0] rowOUT, colOUT, count_clk_OUT
+);
+	reg [DATA_WIDTH-1:0] row, col, count_clk;
+	
+	wire [DATA_WIDTH-1:0] line_Buffer_out;
+	
+	Line_Buffer11x32 Line_Buffer11x32_inst	(	.clk(clk), .reset(reset),
+															.Din(o21),
+															.Dout(line_Buffer_out),
+															.addrOUT()
+														);
+	assign o12 = line_Buffer_out;
+	assign valid_out = ((col%2==1) && (row%2==0) && clk) ? 1'b1 : 1'b0;	
+	assign rowOUT = row;
+	assign colOUT = col;
+	assign count_clk_OUT = count_clk;
+	
+	
+	
+	always @(posedge clk) 
+	begin: STRIDE_2
+	  if (reset) begin
+	    row = 32'd0;
+	    col = 32'd0;
+	    count_clk = 32'd0;
+	  end
+	  else begin
+	    count_clk = count_clk + 32'd1;
+		  if(count_clk >32'd15) begin
+			   col = col + 32'd1;
+			   if(col > 32'd14) begin
+			   	 col = 32'd1;
+				   row = row + 32'd1;
+				   if(row > 32'd13) begin
+					   row = 32'd0;
+				   end
+			   end		
+		   end
+	  end
+	    
+	  
+
+
+	end
+	
+	always @(posedge clk) 
+	begin: BUFFER
+		o22 <= Din;
+		o21 <= o22;
+		o11 <= line_Buffer_out;
+	end
+endmodule	
