14518b0c9ebd256a96b2d1c1ee4739c799f9f7e0
NguyenTienBao <47103241+tienbao08@users.noreply.github.com>
2021-05-29 16:51:38 +0700

    Add files via upload

--- project/RTL/vgg11_hdl.srcs/sources_1/new/conv_224.v
+++ project/RTL/vgg11_hdl.srcs/sources_1/new/conv_224.v
@@ -52,30 +52,17 @@ module conv_224(
                     kernal6,
                     kernal7,
                     kernal8;
-                    
-    wire [31:0] dataOut0, dataOut1, dataOut2, dataOut3, dataOut4, dataOut5, dataOut6, dataOut7, dataOut8;
-                        
-
-assign kernal0 = (counter <= 10'd450 || (rowcounter == 8'd222 && counter == 10'd450)) ? 32'h0 : dataOut0;
-assign kernal1 = (counter <= 10'd449) ? 32'h0 : dataOut1;
-assign kernal2 = (counter <= 10'd449 || counter == 10'd673) ? 32'h0 : dataOut2;
-assign kernal3 = (counter <= 10'd226 || counter == 10'd450 || (rowcounter == 8'd222 && counter == 10'd450)) ? 32'h0 : dataOut3;
-assign kernal4 = (counter < 10'd226) ? 32'h0 : dataOut4;
-assign kernal5 = (counter < 10'd226 || counter == 10'd449 || counter == 10'd673) ? 32'h0 : dataOut5;
-assign kernal6 = (counter <= 10'd226 || counter == 10'd450 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut6;
-assign kernal7 = (counter < 10'd226 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut7;
-assign kernal8 = (counter < 10'd226 || counter == 10'd449 || counter == 10'd673 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut8;
-
+                   
 lineBuffer_conv line0(
-    .dataOut0(dataOut0),
-    .dataOut1(dataOut1),
-    .dataOut2(dataOut2),
-    .dataOut3(dataOut3),
-    .dataOut4(dataOut4),
-    .dataOut5(dataOut5),
-    .dataOut6(dataOut6),
-    .dataOut7(dataOut7),
-    .dataOut8(dataOut8),
+    .dataOut_0(kernal0),
+    .dataOut_1(kernal1),
+    .dataOut_2(kernal2),
+    .dataOut_3(kernal3),
+    .dataOut_4(kernal4),
+    .dataOut_5(kernal5),
+    .dataOut_6(kernal6),
+    .dataOut_7(kernal7),
+    .dataOut_8(kernal8),
     .counter(counter),
     .rowcounter(rowcounter),
     .clk(clk),
--- project/RTL/vgg11_hdl.srcs/sources_1/new/lineBuffer_conv.v
+++ project/RTL/vgg11_hdl.srcs/sources_1/new/lineBuffer_conv.v
@@ -21,15 +21,15 @@
 
 
 module lineBuffer_conv(
-    dataOut0,
-    dataOut1,
-    dataOut2,
-    dataOut3,
-    dataOut4,
-    dataOut5,
-    dataOut6,
-    dataOut7,
-    dataOut8,
+    dataOut_0,
+    dataOut_1,
+    dataOut_2,
+    dataOut_3,
+    dataOut_4,
+    dataOut_5,
+    dataOut_6,
+    dataOut_7,
+    dataOut_8,
     counter,
     rowcounter,
     clk,
@@ -39,10 +39,11 @@ module lineBuffer_conv(
     
     input wire clk, rst;
     input wire [31:0] dataIn;
-    output wire [31:0] dataOut0, dataOut1, dataOut2, dataOut3, dataOut4, dataOut5, dataOut6, dataOut7, dataOut8;
+    output wire [31:0] dataOut_0, dataOut_1, dataOut_2, dataOut_3, dataOut_4, dataOut_5, dataOut_6, dataOut_7, dataOut_8;
     output reg [9:0] counter;
     output reg [7:0] rowcounter;
     
+    wire [31:0] dataOut0, dataOut1, dataOut2, dataOut3, dataOut4, dataOut5, dataOut6, dataOut7, dataOut8;
     reg [7:0] addr0, addr1;
     wire [31:0] dataOut_B0, dataOut_B1;
     
@@ -114,5 +115,15 @@ kernalBuffer_conv k2(
     .dataIn(dataOut_B1),
     .clk(clk)
     );
+
+assign dataOut_0 = (counter <= 10'd450 || (rowcounter == 8'd222 && counter == 10'd450)) ? 32'h0 : dataOut0;
+assign dataOut_1 = (counter <= 10'd449) ? 32'h0 : dataOut1;
+assign dataOut_2 = (counter <= 10'd449 || counter == 10'd673) ? 32'h0 : dataOut2;
+assign dataOut_3 = (counter <= 10'd226 || counter == 10'd450 || (rowcounter == 8'd222 && counter == 10'd450)) ? 32'h0 : dataOut3;
+assign dataOut_4 = (counter < 10'd226) ? 32'h0 : dataOut4;
+assign dataOut_5 = (counter < 10'd226 || counter == 10'd449 || counter == 10'd673) ? 32'h0 : dataOut5;
+assign dataOut_6 = (counter <= 10'd226 || counter == 10'd450 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut6;
+assign dataOut_7 = (counter < 10'd226 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut7;
+assign dataOut_8 = (counter < 10'd226 || counter == 10'd449 || counter == 10'd673 || (rowcounter == 8'd222 && counter >= 10'd450)) ? 32'h0 : dataOut8;
 
 endmodule
