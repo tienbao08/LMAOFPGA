58f262d3b5718d517b86e22688bc2694b670acae
DucNguyen008 <76895207+DucNguyen008@users.noreply.github.com>
2021-06-25 11:58:27 +0700

    Update Read_Pointer_16384.v

--- project/RTL/vgg11_hdl.srcs/sources_1/Combo1_new/Read_Pointer_16384.v
+++ project/RTL/vgg11_hdl.srcs/sources_1/Combo1_new/Read_Pointer_16384.v
@@ -1,5 +1,7 @@
 //REMEMBER: Read counter pointer in has 1 bit larger address of ff_ram, if ADDR_WIDTH = 14, rptr has 15 bit
 //Write_Pointer count 2^14 = 16384
+`timescale 1ns / 1ps 
+
 module Read_Pointer_16384
 
 #(
