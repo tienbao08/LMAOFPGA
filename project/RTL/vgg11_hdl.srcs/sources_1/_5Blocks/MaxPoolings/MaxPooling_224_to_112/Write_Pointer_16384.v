1ae39e546f2e76086d0299f972649660684a895b
DucNguyen008 <76895207+DucNguyen008@users.noreply.github.com>
2021-07-24 15:42:37 +0700

    Update Write_Pointer_16384.v

--- project/RTL/vgg11_hdl.srcs/sources_1/_5Blocks/MaxPoolings/MaxPooling_224_to_112/Write_Pointer_16384.v
+++ project/RTL/vgg11_hdl.srcs/sources_1/_5Blocks/MaxPoolings/MaxPooling_224_to_112/Write_Pointer_16384.v
@@ -13,33 +13,21 @@ module Write_Pointer_16384
 	input fifo_we,
 	output reg [ADDR_WIDTH:0] wptr
 );
+
 	always @(posedge clk)
-	begin
+	begin: RESET_CHECK
 		if (rst) begin
 			wptr <= 15'b0;
-		end	
-		else if(fifo_we)  begin
-			wptr <= wptr + 15'd1;
 		end
+	end
+	always @(negedge clk) 
+	begin: COUNT_WPTR
+		if (fifo_we) begin
+			wptr <= wptr + 15'd1;
+		end	
 		else begin
 			wptr <= wptr;
 		end
 	end
-
-	//always @(posedge clk)
-	//begin: RESET_CHECK
-		//if (rst) begin
-			//wptr <= 15'b0;
-		//end
-	//end
-	//always @(fifo_we) 
-	//begin: COUNT_WPTR
-		//if (fifo_we) begin
-			//wptr <= wptr + 15'd1;
-		//end	
-		//else begin
-			//wptr <= wptr;
-		//end
-	//end
 
 endmodule
